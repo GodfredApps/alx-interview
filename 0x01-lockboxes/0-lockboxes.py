43f99165b78df6951d81b5c38ed5d5898cf1656a
GodfredApps <26176783+GodfredApps@users.noreply.github.com>
2023-07-07 05:26:19 +0000

    global repo updated

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+def canUnlockAll(boxes):
+    total_boxes = len(boxes)
+    unlocked_boxes = [False] * total_boxes
+    unlocked_boxes[0] = True  # The first box is unlocked by default
+    stack = [0]  # Start with the first box
+
+    while stack:
+        current_box = stack.pop()
+        keys = boxes[current_box]
+
+        for key in keys:
+            if key < total_boxes and not unlocked_boxes[key]:
+                unlocked_boxes[key] = True
+                stack.append(key)
+
+    return all(unlocked_boxes)
